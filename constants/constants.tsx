e098946869bdbd55883ec4cf1cb30ba2bfa61c88
MOHAMMED LABEEB A P <labeebap446@gmail.com>
2025-03-23 16:19:48 +0530

    Merge branch 'labeeb'

--- constants/constants.tsx
+++ constants/constants.tsx
@@ -1,1 +1,19 @@
 export const API_URL = 'http://192.168.21.138:4000';
+// import os from 'os';
+
+// function getLocalIPAddress(): string {
+//   const interfaces = os.networkInterfaces();
+//   for (const devName in interfaces) {
+//     const iface = interfaces[devName];
+//     if (iface) {
+//       for (const details of iface) {
+//         if (details.family === 'IPv4' && !details.internal) {
+//           return details.address;
+//         }
+//       }
+//     }
+//   }
+//   return 'localhost';
+// }
+
+// export const API_URL = `http://${getLocalIPAddress()}:4000`;
